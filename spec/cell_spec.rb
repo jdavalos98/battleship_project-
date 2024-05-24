63b0f1c863227950921394f36dc855f5427dd1db
Karl Fallenius <smilodon.p@gmail.com>
2024-05-24 14:29:35 -0600

    updated cell spec

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 RSpec.describe Cell.new do
   before(:each) do
-    @cell = Cell.new("B4")
+    @cell = Cell.new(coordinate)
+    @cell1 = Cell.new("B4")
   end
 
   it "can initialize" do
-    expect(@cell.coordinate).to eq("B4")
+    expect(@cell1.coordinate).to eq("B4")
   end
 end
