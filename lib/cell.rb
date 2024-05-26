6e00007f15733ee36ab66cdea87e2a944f67ed2b
Karl Fallenius <smilodon.p@gmail.com>
2024-05-25 21:25:45 -0600

    cell class and test are functional

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,33 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate, :ship
 
-  def initialize(coordinate)
+  def initialize(coordinate, fired_upon = false)
     @coordinate = coordinate
+    @ship = ship
+    @fired_upon = fired_upon
   end
 
+  def empty?
+    if @ship == nil
+      true
+    else
+      false
+    end
+  end
+
+  def place_ship(ship)
+    if empty? == true  
+      @ship = ship
+      empty? == false
+    end
+  end
+
+  def fire_upon
+    @ship.health -= 1
+    @fired_upon = true
+  end
   
+  def fired_upon?
+    @fired_upon
+  end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,12 +1,31 @@
 require 'spec_helper'
 
-RSpec.describe Cell.new do
+RSpec.describe Cell do
   before(:each) do
-    @cell = Cell.new(coordinate)
     @cell1 = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   it "can initialize" do
     expect(@cell1.coordinate).to eq("B4")
+    expect(@cell1.ship).to eq(nil)
+    expect(@cell1.empty?).to be true
   end
+
+  it "can place a ship" do
+    @cell1.place_ship(@cruiser)
+    expect(@cell1.ship).to eq(@cruiser)
+    expect(@cell1.empty?).to be false
+  end
+
+  it "can be fired upon" do
+    @cell1.place_ship(@cruiser)
+    expect(@cell1.fired_upon?).to be false
+
+    @cell1.fire_upon
+    expect(@cruiser.health).to eq(2)
+    expect(@cell1.fired_upon?).to be true
+  end
+  
+
 end
