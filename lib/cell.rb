f7f4993a7eb1d0844c908820a9bbd95abde7fe67
Karl Fallenius <smilodon.p@gmail.com>
2024-05-26 13:43:04 -0600

    almost complete cell class? test with render

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,11 @@
 class Cell
-  attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship, :place_ship
 
-  def initialize(coordinate, fired_upon = false)
+  def initialize(coordinate, fired_upon = false, reveal_ship = false)
     @coordinate = coordinate
     @ship = ship
     @fired_upon = fired_upon
+    @reveal_ship = reveal_ship
   end
 
   def empty?
@@ -16,18 +17,39 @@ class Cell
   end
 
   def place_ship(ship)
-    if empty? == true  
+    if empty? 
       @ship = ship
-      empty? == false
+    else
+      "Space Occupied"
     end
   end
 
   def fire_upon
-    @ship.health -= 1
     @fired_upon = true
+    if !empty?
+      @ship.health -= 1
+    end
   end
   
   def fired_upon?
     @fired_upon
+  end
+
+  def reveal_ship?
+    @reveal_ship
+  end
+
+  def render(reveal_ship = false) 
+    if !fired_upon? && empty?
+      "."
+    elsif reveal_ship? && !empty? 
+      "S"
+    elsif fired_upon? && empty?
+      "M"
+    elsif fired_upon? && !empty? && !@ship.sunk?
+      "H"
+    elsif @ship.sunk? 
+      "X"
+    end
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe Cell do
   before(:each) do
     @cell1 = Cell.new("B4")
+    @cell2 = Cell.new("C3")
+    @ship = Ship.new(@type, @health)
     @cruiser = Ship.new("Cruiser", 3)
   end
 
@@ -27,5 +29,35 @@ RSpec.describe Cell do
     expect(@cell1.fired_upon?).to be true
   end
   
+  describe "can render cells" do
+    it "can render an empty cell" do
+      expect(@cell1.render).to eq(".")
+    end
 
+    it "can render a missed shot" do
+      @cell1.fire_upon
+      expect(@cell1.render).to eq("M")
+    end
+
+    it "can reveal a ship" do
+      expect(@cell2.render).to eq(".")
+      @cell2.place_ship(@cruiser)
+      expect(@cell2.render(true)).to eq("S")
+    end
+
+    it "can render a succesful shot" do
+      @cell2.place_ship(@cruiser)
+      @cell2.fire_upon
+      expect(@cell2.render(true)).to eq("H")
+      expect(@cruiser.sunk?).to be false
+    end
+    it "can render a sunken ship" do
+      @cell2.place_ship(@cruiser)
+      @cell2.fire_upon
+      @cruiser.hit
+      @cruiser.hit
+      expect(@cruiser.sunk?).to be true
+      expect(@cell2.render).to eq("X")
+    end
+  end
 end
