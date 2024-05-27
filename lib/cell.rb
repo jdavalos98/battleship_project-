90e3d0ca40c225ca95d9d00b002564d7a71e9abc
Joel Davalos <jsdavalos@gmail.com>
2024-05-26 20:03:34 -0600

    Saving my branch progresss '
    '

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,9 +1,31 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate, :ship
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = ship
+   
   end
 
+  def empty?
+    if ship == nil
+      true
+    else
+      false
+    end
+  end
   
-end
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def fired_upon?
+    false
+  end
+
+  def fired_upon
+    @fired_upon = true
+      fired_upon? 
+  end
+end 
+
