980457a3e9a0e2c94cc9cfdf914e2fd484b8d444
Karl Fallenius <smilodon.p@gmail.com>
2024-05-28 10:25:08 -0600

    polished working consecutive_coordinates method

--- lib/board.rb
+++ lib/board.rb
@@ -34,44 +34,23 @@ class Board
     return false unless placement.all?{|coordinate| valid_coordinate?(coordinate)}
     return false unless placement.length == ship.health
     return false unless consecutive_coordinates?(placement)
+    true
+    
     #checks that the cell is empty before placing ship
   end
 
   def consecutive_coordinates?(placement)
     row = placement.map {|cell| cell[0]}
     column = placement.map {|cell| cell[1]}
-
     if row.uniq.length == 1
-      consecutive_row = row.each_cons(2).all? {|a, b| b == a.next}
+      column.each_cons(2).all? {|a, b| b == a.next}
     elsif column.uniq.length == 1
-      consecutive_column = column.each_cons(2).all? {|a, b| b == a.next}
+      row.each_cons(2).all? {|a, b| b == a.next}
     else
       false
     end
   end
+
   
-  # @cells.each_cons(placement.length){|array| p array}
-  # row = letter
-  # column = number
-  # row = placement.map{|cell| cell[0]}
-  # column = placement.map{|cell| cell[1]}
-  # p row
-  # p column
-
-  private 
-
-  def generate_cells
-    coordinates = [
-      "A1", "A2", "A3", "A4",
-      "B1", "B2", "B3", "B4",
-      "C1", "C2", "C3", "C4",
-      "D1", "D2", "D3", "D4"
-      ]
-
-
-    coordinates.each do |coordinate|
-        @cells[coordinate] = Cell.new(coordinate)
-    end
-  end
 end
 
