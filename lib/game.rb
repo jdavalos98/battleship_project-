7d7b56963435afb4b681679e73945ad59bb9bf60
Karl Fallenius <smilodon.p@gmail.com>
2024-05-29 16:11:01 -0600

    added computer chip placement functionality

--- lib/game.rb
+++ lib/game.rb
@@ -8,6 +8,7 @@ class Game
     @computer_submarine = Ship.new("Submarine", 2)
     @player_board = Board.new
     @computer_board = Board.new
+    @cells =  ["A1","A2","A3","A4","B1","B2","B3","B4","C1","C2","C3","C4","D1","D2","D3","D4"]
   end
 
   def run
@@ -33,23 +34,40 @@ class Game
   end
 
   def setup_game
-    place_computer_ships
-    puts"I have laid out my ships on the grid."
+    place_computer_ships(@computer_cruiser, @computer_submarine)
+
+    puts "I have laid out my ships on the grid."
     puts "You now need to lay out your two ships."
     puts "The Cruiser is three units long and the Submarine is two units long."
     puts @player_board.render_board
-    puts "Enter the coordinates for the Cruiser (3 adjacent spaces)"
-    puts @computer_board.render_board(true)
+    puts "Enter the coordinates for the Cruiser (3 adjacent spaces), seperated by commas."
+
+    choice = gets.chomp.upcase
+    case  choice
+    when  choice = @player_board.cells.any?
+      @player_board.place(@player_cruiser, choice)
+    end
+    puts @player_board.render_board(true)
+    
 
     #player ship placement
   end
 
-  def place_computer_ships
-    @computer_board.place(@computer_cruiser, @computer_board.cells)
-    @computer_board.place(@computer_submarine, @computer_board.cells)
+  def place_computer_ships(computer_cruiser, computer_submarine)
+    cruiser_coordinates = random_coordinates(@computer_cruiser, @computer_cruiser.length)
+    sub_coordinates = random_coordinates(@computer_submarine, @computer_submarine.length)
+    @computer_board.place(@computer_cruiser, cruiser_coordinates)
+    @computer_board.place(@computer_submarine, sub_coordinates)
   end
-end
+
+  def random_coordinates(ship, length)
+    loop do
+      coordinates = @cells.sample(length)
+      return coordinates if @computer_board.valid_placement?(ship, coordinates)
+    end
+  end
+
 
 game = Game.new
 game.run
-
+end
