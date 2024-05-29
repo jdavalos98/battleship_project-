4be33bd6bf53935cb778ebb0fb4db698ffa4fa12
Karl Fallenius <smilodon.p@gmail.com>
2024-05-29 11:48:11 -0600

    no new functionality, just an update

--- lib/game.rb
+++ lib/game.rb
@@ -2,8 +2,10 @@ require './lib/game_helper.rb'
 class Game
 
   def initialize
-    @cruiser = Ship.new("Cruiser", 3)
-    @submarine = Ship.new("Submarine", 2)
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_submarine = Ship.new("Submarine", 2)
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
     @player_board = Board.new
     @computer_board = Board.new
   end
@@ -26,17 +28,25 @@ class Game
       setup_game
     when "q"
       exit
+      "Hasta la vista baby"
     end
   end
 
   def setup_game
-    #computer ship placement
+    place_computer_ships
     puts"I have laid out my ships on the grid."
     puts "You now need to lay out your two ships."
     puts "The Cruiser is three units long and the Submarine is two units long."
+    puts @player_board.render_board
+    puts "Enter the coordinates for the Cruiser (3 adjacent spaces)"
+    puts @computer_board.render_board(true)
+
     #player ship placement
+  end
 
-
+  def place_computer_ships
+    @computer_board.place(@computer_cruiser, @computer_board.cells)
+    @computer_board.place(@computer_submarine, @computer_board.cells)
   end
 end
 
